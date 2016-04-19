4d88838ce62f39cc7fba8a88cdebf8a3ad8f91e6
Vivien STEFFEN <vsteffen@e2r3p21.42.fr>
2016-04-19 18:55:51 +0200

    Correct exit_prog due to the modification of ft_color_style

--- srcs/ft_exit_prog.c
+++ srcs/ft_exit_prog.c
@@ -6,17 +6,15 @@
 /*   By: vsteffen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/25 16:54:41 by vsteffen          #+#    #+#             */
-/*   Updated: 2016/03/25 17:32:29 by vsteffen         ###   ########.fr       */
+/*   Updated: 2016/04/19 18:55:15 by vsteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_exit_prog(char *status, int color, int debug)
+void	ft_exit_prog(char *status, char *color, int debug)
 {
-	ft_colors(color);
-	ft_putstr(status);
-	ft_colors(0);
+	ft_colors(color, status);
 	if (debug == 0)
 		perror("\nperror message ");
 	exit(0);
